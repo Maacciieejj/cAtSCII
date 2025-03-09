c2570afd59d6f528cc3bad4b2e250168d3a3fc75
Maacciieejj <maciej.panas@gmail.com>
2025-03-09 15:28:20 +0100

    odzyskanie po awarii z maliny

--- cAtSCII.py
+++ cAtSCII.py
@@ -8,43 +8,86 @@ from links.ogladane import kotyogladane #import asci artow
 from links.nagrobek import nagrobek #import asci artow
 from links.portret import portret #import asci artow
 
-pauza = False
-czas_rozpoczecia_pauzy = 0
-calkowity_czas_pauzy = 0
+
+
 
 def clear_screen(): #  czyszczenie ekran startowy
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+# ponizej definicja funkcji wydarzenia losowe
+
 def wydarzenie_losowe(kot): # definicja funkcji wydarzenie_losowe
     ostatnia_aktualizacja = time.time() # aktualizacja czasu ostatniej aktualizacji by pokazywaly sie aktualne statsy podczas wydarzen losowych
     while True:
-        czas_oczekiwania = random.choice([300, 600, 600, 900, 900, 1200, 1500])  # losuje czas oczekiwania w sekundach
+        czas_oczekiwania = random.choice([600, 900, 900, 1200, 1500])  # losuje czas oczekiwania w sekundach
         time.sleep(czas_oczekiwania)  # czeka wylosowaną liczbę sekund
 
-# aktualizacja statsow przed wydarzeniami
+        # aktualizacja statsow przed wydarzeniami
         teraz = time.time()
         minuty = int((teraz - ostatnia_aktualizacja) // 60)
         if minuty > 0:
-            kot.najedzenie -= minuty / 60
-            kot.zadbanie -= minuty / 60
-            kot.dostatekuwagi -= minuty / 60
+            kot.najedzenie -= minuty / 120 # odejmuje część punkyu na minutę od statystyki ( np minuty / 30 - spadek 2 punktów na godzinę, minuty / 120 - spadek 0.5 punktu na godzinę )
+            kot.zadbanie -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
+            kot.dostatekuwagi -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
             ostatnia_aktualizacja = teraz
 
         if not kot.zyje():  # sprawdź czy kot żyje przed wydarzeniem
             print("\nTwój kot zmarł!") 
             print(nagrobek)
+            kot.zapisz_log("R. I. P.") #zapis logu
             break
-        wydarzenie = random.choice(["biegunka", "smutek", "upolowanie_muchy", "spotkanie_kocich_znajomych", "spotkanie_kocich_znajomych", "szwędanie", "szwędanie", "szwędanie"])
-        getattr(kot, wydarzenie)()  # wywołuje wylosowaną metodę na kocie
+
+        wydarzenie = random.choice(["biegunka", "smutek", "upolowanie_myszy", "spotkanie_kocich_znajomych", "dzieci_głaszcza", "szwedanie"]) # losowanie wydarzenia
+        getattr(kot, wydarzenie)()  # Wywołuje metodę o nazwie wylosowanego wydarzenia na obiekcie kot . Na przykład, jeśli wylosowano "biegunka", to wykona się kot.biegunka()
         print(f"\n\nWydarzenie losowe:{ wydarzenie}")  # pokazuje co się stało
-        kot.zapisz_log(f"Wydarzenie losowe: {wydarzenie}") # zapisuje log
+        kot.zapisz_log(f"Wydarzenie losowe: { wydarzenie}") # zapisuje log
         kot.pokaz_stan()
         print("(enter)")    
 
-#--------------------------------------------------------
+
+# ponizej definicja funkcji drobnych wydarzeń
+
+def drobne_wydarzenia(kot): # definicja funkcji 
+    ostatnia_aktualizacja = time.time() # aktualizacja czasu ostatniej aktualizacji by pokazywaly sie aktualne statsy podczas wydarzen losowych
+    while True:
+        czas_oczekiwania = random.choice([120, 120, 120, 60, 60, 180,])  # losuje czas oczekiwania w sekundach
+        time.sleep(czas_oczekiwania)  # czeka wylosowaną liczbę sekund
+
+        # aktualizacja statsow przed wydarzeniami
+        teraz = time.time()
+        minuty = int((teraz - ostatnia_aktualizacja) // 60) 
+        if minuty > 0:
+            kot.najedzenie -= minuty / 120 # odejmuje część punkyu na minutę od statystyki ( np minuty / 30 - spadek 2 punktów na godzinę, minuty / 120 - spadek 0.5 punktu na godzinę )
+            kot.zadbanie -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
+            kot.dostatekuwagi -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
+            ostatnia_aktualizacja = teraz
+
+        if not kot.zyje():  # sprawdź czy kot żyje przed wydarzeniem
+            print("\nTwój kot zmarł!") 
+            print(nagrobek)
+            kot.zapisz_log("R. I. P.") #zapis logu
+            break
+
+        wydarzenie = random.choice(["szwedanie","przeciaganie", "upadek_z_krzesla", "mruczenie", "kaslanie", "upolowanie_muchy", "gonitwa_po_meblach", "upolowanie_pajaka", "wycieczka_na_dach", "wizyta_kota_sasiada", "utkniecie_pod_zlewem", "spotkanie_z_jezem", "koci_marazm"]) # losowanie wydarzenia
+        getattr(kot, wydarzenie)()  # Wywołuje metodę o nazwie wylosowanego wydarzenia na obiekcie kot . Na przykład, jeśli wylosowano "biegunka", to wykona się kot.biegunka()
+        print(f"\n\nDrobne wydarzenie:{ wydarzenie}")  # pokazuje co się stało
+        kot.zapisz_log(f"Drobne wydarzenie:{ wydarzenie}") # zapisuje log
+        kot.pokaz_stan()
+        print("(enter)") 
+
+
+
+
+
+
+# poiżej klasa Cat -----------------------------------
 
 
 class Cat: 
+
+
+
     #poniżej konstruktor klasy Cat
     def __init__(self): # konstruktor klasy Cat
         self.najedzenie = 10 #Tworzy zmienną przechowującą poziom najedzenia kota. Self oznacza, że zmienna należy do instancji klasy
@@ -52,7 +95,11 @@ class Cat:
         self.dostatekuwagi = 10
         self.moment_adopcji = time.time()  # dodajemy czas startu jako właściwość kota
 
-# funkcja zapisu logów do pliku
+
+
+
+
+    # funkcja zapisu logów do pliku
     def zapisz_log(self, akcja):
         with open('koci_pamietnik.txt', 'a', encoding='utf-8') as plik:
             czas_gry = int(time.time() - self.moment_adopcji)
@@ -68,7 +115,9 @@ class Cat:
             plik.write(log)
 
 
-# ponizej są metody, które zmieniają wartość statystyk kota
+
+
+    # ponizej są metody, które zmieniają wartość statystyk kota
     def karmienie(self): # definicja metody karmienie w klasie Cat
         self.najedzenie = min(10, self.najedzenie + 2)
         
@@ -81,31 +130,88 @@ class Cat:
     def zyje(self):
         return self.najedzenie > 0 and self.zadbanie > 0 and self.dostatekuwagi > 0
 
-# ponizej metody - wydarzenia losowe
+
+
+
+    # ponizej metody - wydarzenia losowe (wieksze )
+
     def biegunka(self):
         self.najedzenie = max(0, self.najedzenie - 2) 
         self.zadbanie = max(0, self.zadbanie - 2) 
 
+    def upolowanie_myszy(self):
+        self.najedzenie = min(10, self.najedzenie + 2)
+
+    def dzieci_głaszcza(self):
+        self.zadbanie = min(10, self.zadbanie + 2)
+
+
+
     def smutek(self):
         self.dostatekuwagi = max(0, self.dostatekuwagi - 2)
 
-    def upolowanie_muchy(self):
-        self.najedzenie = min(10, self.najedzenie + 1)
+    def spotkanie_kocich_znajomych(self):
+        self.dostatekuwagi = min(10, self.dostatekuwagi + 2)
 
-    #def mycie_futra(self): wyłączone
-        #self.dostatekuwagi = min(10, self.dostatekuwagi + 1) wyłączone
 
-    def szwędanie(self):
+    def szwedanie(self):
         pass  # ta metoda celowo nic nie robi
 
-    def spotkanie_kocich_znajomych(self):
-        self.dostatekuwagi = min(10, self.dostatekuwagi + 1)
 
 
- #wyswietla menu i aktualizuje stan kota
+    # ponizej metody - drobne wydarzenia
+
+    # zadbanie
+
+    def przeciaganie(self):
+        self.zadbanie = min(10, self.zadbanie + 0.25)  
+
+    def upadek_z_krzesla(self):
+        self.zadbanie = max(0, self.zadbanie - 0.25)  
+
+    def mruczenie(self):
+        self.zadbanie = min(10, self.zadbanie + 0.25)  
+
+    def kaslanie(self):
+        self.zadbanie = max(0, self.zadbanie - 0.25) 
+
+
+    # najedzenie
+
+    def upolowanie_muchy(self):
+        self.najedzenie = min(10, self.najedzenie + 0.25)  
+
+    def gonitwa_po_meblach(self):
+        self.najedzenie = max(0, self.najedzenie - 0.25) 
+
+    def upolowanie_pajaka(self):
+        self.najedzenie = min(10, self.najedzenie + 0.25)  
+
+    def wycieczka_na_dach(self):
+        self.najedzenie = max(0, self.najedzenie - 0.25) 
+
+
+    # dostatekuwagi
+
+    def wizyta_kota_sasiada(self):
+        self.dostatekuwagi = min(10, self.dostatekuwagi + 0.25)  
+
+    def utkniecie_pod_zlewem(self):
+        self.dostatekuwagi = max(0, self.dostatekuwagi - 0.25) 
+
+    def spotkanie_z_jezem(self):
+        self.dostatekuwagi = min(10, self.dostatekuwagi + 0.25)  
+
+    def koci_marazm(self):
+        self.dostatekuwagi = max(0, self.dostatekuwagi - 0.25) 
+
+
+
+
+    #wyswietla menu i aktualizuje stan kota
     def pokaz_stan(self):
         # ponizej konstruowanie licznika czasu gry
-        czas_gry = int(time.time() - self.moment_adopcji - calkowity_czas_pauzy)  # całkowity czas w sekundach z odjetym całkowitego czasu pauzy
+        czas_gry = int(time.time() - self.moment_adopcji)  # całkowity czas w sekundach
         minuty = (czas_gry // 60) % 60
         godziny = (czas_gry // 3600) % 24
         dni = czas_gry // 86400
@@ -120,10 +226,13 @@ class Cat:
 
 
 
-#--------------ponizej jest główna funkcja programu ----------
+
+#--------------ponizej jest GŁÓWNA FUNKCJA PROGRAMU -----------------------------
+
 
 
 def main():
+
     # To się wykonuje RAZ na początku programu
     kot = Cat() # tutaj wywołuje się __init__
     print(portret)
@@ -132,30 +241,45 @@ def main():
     ostatnia_aktualizacja = time.time()
 
 
-    # Uruchom wątek wydarzeń losowych
+
+    # Ponizej wątki - funkcje, które będą działać równolegle z główną funkcją programu
+
+    # Uruchom WĄTEK wydarzeń losowych
     watek_wydarzen = threading.Thread(target=wydarzenie_losowe, args=(kot,)) #Tworzy nowy wątek i mówi, jaką funkcję ma wykonywać i rzekazuje argumenty do tej funkcj - tu kot
     watek_wydarzen.daemon = True  # wątek zakończy się gdy program się zakończy. "daemon" (usługowy)
     watek_wydarzen.start() #Uruchamia wątek. od tąd działa równolegle z głównym programem
 
 
-    # While To jest pętla, która się powtarza, tak dlugo jak spelniony jest warunek
+    # Uruchom WĄTEK drobnych wydarzeń
+    watek_drobnych = threading.Thread(target=drobne_wydarzenia, args=(kot,)) #Tworzy nowy wątek i mówi, jaką funkcję ma wykonywać i rzekazuje argumenty do tej funkcj - tu kot
+    watek_drobnych.daemon = True  # wątek zakończy się gdy program się zakończy. "daemon" (usługowy)
+    watek_drobnych.start() #Uruchamia wątek. od tąd działa równolegle z głównym programem
+
+
+
+
+
+    # ----- JAKBY GŁÓWNA PETLA PROGRAMU ---- , While To jest pętla, która się powtarza, tak dlugo jak spelniony jest warunek
+
     while kot.zyje():
       
 
+
         # Aktualizacja statystyk
         teraz = time.time() #aktualizacja czasu by pokazywaly się aktualne statystyki pod wydarzeniami losowymi
-        if not pauza:  # obliczaj czas tylko gdy nie ma pauzy
-            minuty = int((teraz - ostatnia_aktualizacja) // 60)  # ile minut minęło - dotyczy opadania statystyk
-            if minuty > 0:
-                kot.najedzenie -= minuty / 60  # odejmij 1/60 punktu za każdą minutę
-                kot.zadbanie -= minuty / 60
-                kot.dostatekuwagi -= minuty / 60
-                ostatnia_aktualizacja = teraz #???
+        minuty = int((teraz - ostatnia_aktualizacja) // 60)  # ile minut minęło - dotyczy opadania statystyk
+        if minuty > 0:
+            kot.najedzenie -= minuty / 120  # odejmuje część punkyu na minutę od statystyki
+            kot.zadbanie -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
+            kot.dostatekuwagi -= minuty / 120 # odejmuje część punkyu na minutę od statystyki
+            ostatnia_aktualizacja = teraz #???
 
         if not kot.zyje(): #jesli zyje jest false to wyświetla się to
             print("\nTwój kot zmarł!") 
             print(nagrobek)
+            kot.zapisz_log("R. I. P.") #zapis logu
             break
+
 
 
         # Wyświetl stan i menu
@@ -163,14 +287,14 @@ def main():
         print("\n1 - Nakarm kota")
         print("2 - Pogłaszcz kota")
         print("3 - Spójrz na kota")
-        print("4 - Połóż kota spać") 
         print("0 - Wyjdź z gry")
         
         wybor = input("?: ") #wyswietla linie i zapisuje co wpisał użytkownik
 
-        # clear_screen() - - wylaczone czyszczenie ekranu
 
-# Sprawdzenie wyboru użytkownika
+
+
+        # Sprawdzenie wyboru użytkownika
         if wybor == "1":
             kot.karmienie()
             kot.zapisz_log("Akcja: Karmienie kota") #zapis logu
@@ -193,16 +317,8 @@ def main():
             print(random.choice(kotyogladane))# wybiera losowo jedną z listy
             print("\nWidzisz kota")
 
-        elif wybor == "4": # pauza
-            global pauza, czas_rozpoczecia_pauzy, calkowity_czas_pauzy
-            pauza = not pauza
-            if pauza:
-                print("\nKot idzie spać...")
-                czas_rozpoczecia_pauzy = time.time()
-            else:
-                print("\nKot się budzi!")
-                calkowity_czas_pauzy += time.time() - czas_rozpoczecia_pauzy
-                ostatnia_aktualizacja = time.time()  # aktualizuj czas po pauzie    
+
+
 
 
 
